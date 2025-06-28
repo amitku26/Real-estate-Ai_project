dabc0b20f9bcfcc2ed8b8fead524f5a5b5812cf7
amitkumar2026 <amitkumar829448@gmail.com>
2025-06-28 22:06:20 +0530

    fix some error

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_cors import CORS
 from routes.predict import predict_bp
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -9,4 +10,6 @@ app.register_blueprint(predict_bp, url_prefix="/api")
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
 
+port = int(os.environ.get("PORT", 5000))  # Use 5000 locally as default
+app.run(host="0.0.0.0", port=port)
 
